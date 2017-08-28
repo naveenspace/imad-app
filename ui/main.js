7f0b29d1a4dad65a86e0e1cbd4533801637a9440
naveenspace <30807082+naveenspace@users.noreply.github.com>
2017-08-28 08:38:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,23 @@
-console.log('loaded!');
+var button = document.getElementById('counter');
 
-var element = document.getElementById('real');
-
-element.innerHTML = 'SUN';
+button.onclick = fucntion(){
+    
+    
+    var requeest = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if(request.readyState == XMLHttpRequest.DONE){
+            if(request.status == 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+                
+            }
+        }
+    };
+    
+    request.open('GET', 'http://naveen714ronaldo.imad.hasura-app.io',true);
+    request.send(null);
+    
+    
+}
